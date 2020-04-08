0a87c32747be17dbd14ead12ec5d0f22936a49b1
lauriegriffiths <laurieinjapan@gmail.com>
2020-04-09 00:09:33 +0900

    Fixed name not deleted bug

--- src/namelist.js
+++ src/namelist.js
@@ -3,9 +3,7 @@ import { List, ListItem } from "@chakra-ui/core";
 
 export class NameList extends React.Component {
   render() {
-    var list_items = this.props.names.map(name => (
-      <ListItem key={name.concat(this.props.groupNumber)}>{name}</ListItem>
-    ));
+    var list_items = this.props.names.map(name => <ListItem>{name}</ListItem>);
     return <List>{list_items}</List>;
   }
 }
